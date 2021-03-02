8ea844c3a766b6b53fe5c524a71a05afe98d4ef1
Alexandre <alexandre.beltz92@gmail.com>
2021-03-02 19:52:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mareu/ui/MeetingListActivity.java
@@ -0,0 +1,81 @@
+package com.example.mareu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.mareu.R;
+import com.example.mareu.di.DI;
+import com.example.mareu.model.Meeting;
+import com.example.mareu.service.MeetingApiService;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MeetingListActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    FloatingActionButton mFab;
+    MeetingApiService meetingApiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting_list);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.toolbar_dark));
+
+        mFab = findViewById(R.id.add_meeting);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        meetingApiService = DI.getMeetingApiService();
+
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter(meetingApiService.getMeeting());
+        mRecyclerView.setAdapter(myRecyclerViewAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        myRecyclerViewAdapter.setListener(new MyRecyclerViewAdapter.ItemClickListener() {
+            @Override
+            public void OnItemClicked(Meeting meeting) {
+                meetingApiService.deleteMeeting(meeting);
+                mRecyclerView.getAdapter().notifyDataSetChanged();
+                Snackbar.make(mRecyclerView, "Reunion supprimée : " + meeting.getSubject(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                meetingApiService.createMeeting(Meeting.random());
+                mRecyclerView.getAdapter().notifyDataSetChanged();
+                Snackbar.make(v, "Réunion ajoutée :" + meetingApiService.getMeeting().size(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+
+        MenuItem item = menu.findItem(R.id.filter);
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/mareu/ui/MyRecyclerViewAdapter.java
@@ -0,0 +1,121 @@
+package com.example.mareu.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mareu.R;
+import com.example.mareu.model.Meeting;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder> {
+
+    private List<Meeting> mMeeting;
+
+    public MyRecyclerViewAdapter(List<Meeting> items) {
+        mMeeting = items;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.recyclerview_item_meeting, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Meeting meeting = mMeeting.get(position);
+        holder.mSubject.setText(meeting.getSubject());
+        holder.mParticipant.setText(meeting.getParticipants());
+        holder.mLocalisation.setText(meeting.getLocalisation());
+        String[] mDate = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.FRANCE).format(meeting.getDate()).split(" ");
+        holder.mDate.setText(mDate[mDate.length - 1]);
+        setRoomColor(holder, position);
+
+        holder.mDeleteMeeting.setOnClickListener(v -> {
+            listener.OnItemClicked(mMeeting.get(position));
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMeeting.size();
+    }
+
+    private void setRoomColor(@NonNull MyViewHolder holder, int position) {
+        switch (position) {
+            case 1:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.ZeusRoom));
+                break;
+            case 2 :
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.HadesRoom));
+                break;
+            case 3:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.AresRoom));
+                break;
+            case 4 :
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.ApolloRoom));
+                break;
+            case 5:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.AthenaRoom));
+                break;
+            case 6:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.PoseidonRoom));
+                break;
+            case 7:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.HeraRoom));
+                break;
+            case 8:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.HermesRoom));
+                break;
+            case 9:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.DionysusRoom));
+                break;
+            case 10:
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.HeraclesRoom));
+                break;
+        }
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mSubject, mDate, mLocalisation, mParticipant;
+        ImageView mAvatar;
+        ImageButton mDeleteMeeting;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mSubject = itemView.findViewById(R.id.subject_meeting);
+            mDate = itemView.findViewById(R.id.date_meeting);
+            mLocalisation = itemView.findViewById(R.id.localisation_meeting);
+            mParticipant = itemView.findViewById(R.id.participant_meeting);
+            mAvatar = itemView.findViewById(R.id.avatar_meeting);
+            mDeleteMeeting = itemView.findViewById(R.id.item_list_delete_button);
+        }
+
+    }
+
+    private ItemClickListener listener;
+
+    public void setListener(ItemClickListener itemClickListener) {
+        this.listener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void OnItemClicked(Meeting meeting);
+    }
+}
