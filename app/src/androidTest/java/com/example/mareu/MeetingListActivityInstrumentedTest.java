06fd1394e143cbc160f66ad6154771b309689f4c
Alexandrebeltz92 <75671812+Alexandrebeltz92@users.noreply.github.com>
2021-04-07 14:58:27 +0200

    V55
    test create new meeting ui

--- app/src/androidTest/java/com/example/mareu/MeetingListActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mareu/MeetingListActivityInstrumentedTest.java
@@ -194,6 +194,111 @@ public class MeetingListActivityInstrumentedTest {
         appCompatImageButton.perform(click());
     }
 
+    @Test
+    public void createNewMeetingWithSuccess() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.add_meeting), withContentDescription("Add meeting button"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction floatingActionButton2 = onView(
+                allOf(withId(R.id.create_meeting),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                3),
+                        isDisplayed()));
+        floatingActionButton2.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.subject_meeting_add),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("sujet"), closeSoftKeyboard());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spinner_room),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                3),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(2);
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction appCompatMultiAutoCompleteTextView = onView(
+                allOf(withId(R.id.tv_participants),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        appCompatMultiAutoCompleteTextView.perform(replaceText("em"), closeSoftKeyboard());
+
+        DataInteraction appCompatTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatMultiAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.tv_participants), withText("em"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        appCompatMultiAutoCompleteTextView2.perform(replaceText("emily@gmail.com, "));
+
+        ViewInteraction appCompatMultiAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.tv_participants), withText("emily@gmail.com, "),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        appCompatMultiAutoCompleteTextView3.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.add_meeting_button_create),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_meeting_list),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                7),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+    }
+
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
